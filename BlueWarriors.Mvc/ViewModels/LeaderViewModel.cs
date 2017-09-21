14fe03ba17d012b5da747f0f024f30f963f7d940
Joseph Serunjogi <joseph.serunjogi@hotmail.com>
2017-09-21 08:34:03 +0300

    Model for the leader views.

---
+++ BlueWarriors.Mvc/ViewModels/LeaderViewModel.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace BlueWarriors.Mvc.ViewModels
+{
+    public class LeaderViewModel
+    {
+        public int LeaderId { get; set; }
+        public string Name { get; set; }
+        public int Msisdn { get; set; }
+        public int RegionId { get; set; }
+        public string Region { get; set; }
+        public int DepartmentId { get; set; }
+        public string Department { get; set; }
+        public int LeaderTypeId { get; set; }
+        public string LeaderType { get; set; }
+        public string Status { get; set; }
+        public DateTime? DeactivationDate { get; set; }
+    }
+}
