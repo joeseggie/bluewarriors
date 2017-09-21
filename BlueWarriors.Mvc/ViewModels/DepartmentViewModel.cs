a64d5899367f7f949a645d4289e752aa019e3522
Joseph Serunjogi <joseph.serunjogi@hotmail.com>
2017-09-21 07:48:35 +0300

    Model for the department views

---
+++ BlueWarriors.Mvc/ViewModels/DepartmentViewModel.cs
@@ -0,0 +1,8 @@
+namespace Bluewarriors.Mvc.ViewModels
+{
+    public class DepartmentViewModel
+    {
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+    }
+}
